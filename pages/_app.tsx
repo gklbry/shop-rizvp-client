d3e839561cc6a6fea4d51db68f9d95c67ba02af6
gklbry <maxhvor4@mail.ru>
2023-12-20 00:05:16 +0500

    create client

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import NextNProgress from 'nextjs-progressbar'
+import 'react-toastify/dist/ReactToastify.css'
+import '@/styles/globals.css'
+import { withHydrate } from 'effector-next'
+
+
+const enhance = withHydrate()
+
+function App({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    mounted && (
+      <>
+        <NextNProgress />
+        <Component {...pageProps} />
+        <ToastContainer
+          position="bottom-right"
+          hideProgressBar={false}
+          closeOnClick
+          rtl={false}
+          limit={1}
+          theme="light"
+        />
+      </>
+    )
+  )
+}
+
+export default enhance(App as React.FC<AppProps>)
